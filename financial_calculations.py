fcab09c0489897b63d46f419e4bfb19057b856fa
Bill729 <s11050003@gmail.com>
2023-10-12 03:43:32 -0400

    Add REST API Endpoints for Fetching Financial Indicators and Stock Data
    Title: Add REST API Endpoints for Fetching Financial Indicators and Stock Data
    
    Body:
    
    This commit introduces two key functionalities to the application by adding REST API endpoints:
    
    1. **Financial Indicators Endpoint (`/get_indicators`)**
        - This endpoint is accessible via GET request at `/get_indicators`.
        - Calls the `calculate_indicators` function from the `financial_calculations` module.
        - The function retrieves financial indicators such as SMA, EMA, RSI, and more for various stocks.
        - Data is returned as a Pandas DataFrame, which is then converted to JSON format for the API response.
    
    2. **Stock Data Endpoint (`/get_stock_data`)**
        - Accessible via GET request at `/get_stock_data`.
        - Calls the `fetch_stock_data` function from the `financial_calculations` module.
        - Fetches various attributes for different stock tickers like market cap, sector, dividend yield, etc.
        - Data is returned as a Python dictionary, which is also converted to JSON format for the API response.
    
    **Changes in Code Structure**
    
    - Introduced Flask library for web application functionalities.
    - Utilized the `jsonify` function from Flask to convert Python data structures to JSON response objects.
    - Included thread pool execution to parallelize data fetching for efficiency.
    
    **Code Files Affected**
    
    - `app.py`: The main Flask application file where the endpoints are defined.
    - `financial_calculations.py`: The module that contains the financial data fetching and processing logic.
    
    These features significantly enhance the data retrieval capabilities of our financial analytics application, providing a programmable interface to access valuable stock market data and indicators.

--- financial_calculations.py
+++ financial_calculations.py
@@ -1,90 +1,122 @@
-# Import necessary libraries.
-import yfinance as yf  # For fetching financial data.
-import pandas as pd  # For data manipulation and analysis.
-import numpy as np  # For numerical operations.
+# Import necessary libraries
+import yfinance as yf   # yfinance is used for fetching stock market data from Yahoo Finance.
+import pandas as pd     # pandas is used for data manipulation and handling.
+import numpy as np      # numpy is used for numerical operations.
+from concurrent.futures import ThreadPoolExecutor  # For executing calls asynchronously.
 
+# List of stock ticker symbols that we're interested in.
+tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "BRK-B", "V", "JNJ", "WMT", "PG"]
+
+# Define a function to fetch data for a single stock ticker.
+def fetch_single_stock_data(ticker):
+
+    # Create an object for the stock using yfinance's Ticker class.
+    stock = yf.Ticker(ticker)
+    
+    # Fetch all available stock information.
+    stock_info = stock.info
+    
+    # Define the specific metrics we want to extract.
+    metrics = [
+        "marketCap", "sector", "industry",
+        "dividendYield", "trailingPE", "earningsQuarterlyGrowth",
+        "currentPrice", "regularMarketVolume", "beta",
+        "fiftyTwoWeekLow", "fiftyTwoWeekHigh", "currency"
+    ]
+    
+    # Create a dictionary to hold our chosen metrics for this stock.
+    # If a metric is not available, we use "N/A" as a placeholder.
+    return {ticker: {metric: stock_info.get(metric, "N/A") for metric in metrics}}
+
+# Define a function to fetch data for all listed stock tickers.
+def fetch_stock_data():
+
+    # Use ThreadPoolExecutor to run multiple threads and speed up data fetching.
+    with ThreadPoolExecutor() as executor:
+        # Fetch the stock data asynchronously and convert the results to a list.
+        results = list(executor.map(fetch_single_stock_data, tickers))
+    
+    # Initialize an empty dictionary to hold all stock data.
+    stock_data_dict = {}
+    
+    # Populate the stock_data_dict with data fetched by all threads.
+    for result in results:
+        stock_data_dict.update(result)
+
+    # Return the dictionary containing all stock data.
+    return stock_data_dict
+
+# Define a function to calculate stock indicators for a single stock ticker.
+def calculate_single_indicator(ticker, close_prices):
+
+    # Calculate Simple Moving Average (SMA) over a 20-day window.
+    sma = close_prices[ticker].rolling(window=20).mean()
+    
+    # Calculate Exponential Moving Average (EMA) over a 20-day window.
+    ema = close_prices[ticker].ewm(span=20).mean()
+    
+    # Calculate Standard Deviation over a 20-day window.
+    rolling_std = close_prices[ticker].rolling(window=20).std()
+    
+    # Calculate the upper and lower Bollinger Bands.
+    upper_band = sma + (rolling_std * 2)
+    lower_band = sma - (rolling_std * 2)
+    
+    # Calculate the daily price change.
+    delta = close_prices[ticker].diff()
+    
+    # Calculate the gain and loss components of RSI.
+    gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
+    
+    # Calculate Relative Strength (RS).
+    rs = gain / loss
+    
+    # Calculate Relative Strength Index (RSI).
+    rsi = 100 - (100 / (1 + rs))
+    
+    # Calculate MACD and Signal line indicators.
+    short_ema = close_prices[ticker].ewm(span=12).mean()
+    long_ema = close_prices[ticker].ewm(span=26).mean()
+    macd = short_ema - long_ema
+    signal_line = macd.ewm(span=9).mean()
+
+    # Compile all these indicators into a single DataFrame.
+    return pd.DataFrame({
+        f'{ticker}_SMA': sma,
+        f'{ticker}_EMA': ema,
+        f'{ticker}_Upper_Band': upper_band,
+        f'{ticker}_Lower_Band': lower_band,
+        f'{ticker}_RSI': rsi,
+        f'{ticker}_MACD': macd,
+        f'{ticker}_Signal_Line': signal_line
+    })
+
+# Define a function to calculate stock indicators for all listed stock tickers.
 def calculate_indicators():
-    # Define the stock tickers for the top 10 S&P 500 companies.
-    tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "BRK-B", "V", "JNJ", "WMT", "PG"]
 
-    # Fetch historical stock data for these tickers for the past 1 year.
+    # Download historical stock data for all tickers for the past 1 year.
     data = yf.download(tickers, period="1y")
-
-    # Extract the 'Close' prices from the fetched data.
+    
+    # Extract only the 'Close' prices from the downloaded data.
     close_prices = data['Close']
+    
+    # Use ThreadPoolExecutor to speed up the indicator calculations.
+    with ThreadPoolExecutor() as executor:
+        # Calculate the indicators for all stock tickers.
+        indicators_list = list(executor.map(lambda ticker: calculate_single_indicator(ticker, close_prices), tickers))
 
-    # Perform basic statistical analysis on the close prices.
-    # Get summary statistics like mean, median, etc.
-    stats_summary = close_prices.describe()
-
-    # Calculate the mean of the close prices.
-    mean = close_prices.mean()
-
-    # Calculate the median of the close prices.
-    median = close_prices.median()
-
-    # Calculate the standard deviation of the close prices.
-    std_dev = close_prices.std()
-
-    # Calculate the variance of the close prices.
-    variance = close_prices.var()
-
-    # Calculate the correlation matrix to understand relationships between different stocks.
-    correlation_matrix = close_prices.corr()
-
-    # Initialize an empty DataFrame to hold all the calculated financial indicators.
-    indicators_df = pd.DataFrame()
-
-    # Loop through each stock ticker to calculate financial indicators
-    for ticker in tickers:
-        # Calculate 20-day Simple Moving Average (SMA).
-        # SMA is the average stock price over the last 'n' days. It's used to identify trends.
-        sma = close_prices[ticker].rolling(window=20).mean()
-        
-        # Calculate 20-day Exponential Moving Average (EMA).
-        # EMA is similar to SMA but gives more weight to recent prices. Useful for tracking short-term price movements.
-        ema = close_prices[ticker].ewm(span=20).mean()
-        
-        # Calculate Bollinger Bands.
-        # Upper and Lower Bollinger Bands are used to identify the volatility and direction of price movement.
-        # They are two standard deviations away from the SMA.
-        rolling_std = close_prices[ticker].rolling(window=20).std()
-        upper_band = sma + (rolling_std * 2)  # Upper Bollinger Band.
-        lower_band = sma - (rolling_std * 2)  # Lower Bollinger Band.
-        
-        # Calculate Relative Strength Index (RSI).
-        # RSI is a momentum indicator that measures the speed and change of price movements.
-        # RSI ranges from 0 to 100. Generally, an RSI above 70 indicates an overbought condition, while an RSI below 30 indicates an oversold condition.
-        delta = close_prices[ticker].diff()  # Calculate price change.
-        gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()  # Average gain over 14 days
-        loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()  # Average loss over 14 days
-        rs = gain / loss  # Calculate Relative Strength (RS).
-        rsi = 100 - (100 / (1 + rs))  # Calculate RSI.
-        
-        # Calculate Moving Average Convergence Divergence (MACD).
-        # MACD is a trend-following momentum indicator, showing the relationship between two EMAs of a security’s price.
-        # The MACD is calculated by subtracting the 26-day EMA from the 12-day EMA.
-        short_ema = close_prices[ticker].ewm(span=12).mean()  # Short-term EMA.
-        long_ema = close_prices[ticker].ewm(span=26).mean()  # Long-term EMA.
-        macd = short_ema - long_ema  # MACD line.
-        signal_line = macd.ewm(span=9).mean()  # Signal line.
-        
-        # Store these indicators in a DataFrame.
-        indicators = pd.DataFrame({
-            f'{ticker}_SMA': sma,
-            f'{ticker}_EMA': ema,
-            f'{ticker}_Upper_Band': upper_band,
-            f'{ticker}_Lower_Band': lower_band,
-            f'{ticker}_RSI': rsi,
-            f'{ticker}_MACD': macd,
-            f'{ticker}_Signal_Line': signal_line
-        })
-        
-        # Add the indicators for this stock to the master DataFrame.
-        indicators_df = pd.concat([indicators_df, indicators], axis=1)
-
-    # Drop the rows that don't have all indicators calculated (first 19 rows).
+    # Combine the calculated indicators for all tickers into a single DataFrame.
+    indicators_df = pd.concat(indicators_list, axis=1)
     indicators_df = indicators_df.iloc[19:]
 
-    # Return the DataFrame with all the indicators.
+    # Return the DataFrame containing all the calculated indicators.
     return indicators_df
+
+if __name__ == "__main__":
+    # This block allows for this script to be run standalone for testing, 
+    # but will not run when imported by Flask.
+    stock_data = fetch_stock_data()
+    stock_indicators = calculate_indicators()
+    print(stock_indicators)
+    print(stock_data)
--- main.py
+++ main.py
@@ -1,15 +1,50 @@
+# Import necessary modules from Flask library and your own financial_calculations script
 from flask import Flask, jsonify, json
-from financial_calculations import calculate_indicators  # Import the function
+from financial_calculations import calculate_indicators, fetch_stock_data
 
+# Create a new Flask web application. '__name__' is a special variable 
+# that gets the Python script’s name when the script is executed.
 app = Flask(__name__)
 
+# Define a URL route for fetching financial indicators. 
+# When you visit http://<server_address>:<port>/get_indicators, 
+# this function will be executed.
 @app.route('/get_indicators', methods=['GET'])
 def get_indicators():
-    indicators_df = calculate_indicators()  # Call the function to get indicators DataFrame
+
+    # Call the 'calculate_indicators' function from your financial_calculations script.
+    # This function returns a DataFrame containing various financial indicators.
+    indicators_df = calculate_indicators()
     
+    # Convert the DataFrame to JSON format. 
+    # 'orient="split"' specifies how the data should be oriented in the JSON output.
     indicators_json = indicators_df.to_json(orient='split')
     
-    # Convert to a Flask Response object
+    # Convert the JSON data into a Flask Response object so it can be sent to the front-end.
     response = jsonify(indicators_json)
-
+    
+    # Return the Flask Response object.
     return response
+
+# Define another URL route for fetching general stock data.
+# When you visit http://<server_address>:<port>/get_stock_data, 
+# this function will be executed.
+@app.route('/get_stock_data', methods=['GET'])
+def get_stock_data():
+    # Call the 'fetch_stock_data' function from your financial_calculations script.
+    # This function returns a dictionary containing various data points for different stocks.
+    stock_data = fetch_stock_data()
+    
+    # Convert the dictionary into a Flask Response object so it can be sent to the front-end.
+    response = jsonify(stock_data)
+    
+    # Return the Flask Response object.
+    return response
+
+# This block is a standard piece of code in Flask.
+# It means: only run the application if this script is executed directly 
+# (not imported as a module in another script).
+if __name__ == '__main__':
+    # Run the Flask application.
+    # The 'debug=True' allows for debugging capabilities.
+    app.run(debug=True)
