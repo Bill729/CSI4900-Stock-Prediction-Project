211ddca3f5bd110fd37b79c8deca401c82d134a5
Ali <ali.ottawa.canada@gmail.com>
2023-12-05 17:37:23 -0500

    News design changed

--- frontend/src/pages/News.vue
+++ frontend/src/pages/News.vue
@@ -1,18 +1,20 @@
 <template>
-  <div style="min-width: auto; min-height: auto; display: grid; grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));">
+  <div style="display: flex; flex-direction: column;">
     <div class="news-item" v-for="newsItem in news" :key="newsItem.url">
+      <h1 class="card-title">Viewing news about {{ this.$store.getters.getSelectedTickerData().info.company_name }} ({{ this.$store.getters.getSelectedTickerData().name }})</h1>
       <div class="img-section">
         <a :href="newsItem.url">
           <img :src="newsItem.urlToImage" :alt="'Photo of article titled: ' + newsItem.title" />
         </a>
       </div>
       <div id="text-section">
-        <a :href="newsItem.url">
+        <a :href="newsItem.url" target="_blank">
           <h4 class="card-title">{{ newsItem.title }}</h4>
         </a>
-        <h5 class="card-title">Published on {{ newsItem.publishedAt.replace('T', ' ').replace('Z', ' ') }}</h5>
-        <h5 class="card-title">Published by {{ newsItem.author }}</h5>
-        <h6 class="card-category">From {{ newsItem.source }}</h6>
+        <h5 class="card-category">Published on {{ newsItem.publishedAt.replace('T', ' ').replace('Z', ' ') }}</h5>
+        <h5 class="card-category">Published by {{ newsItem.author }}</h5>
+        <h6 class="card-title">From {{ newsItem.source }}</h6>
+        <button type="button" class="btn btn-primary"><a class="text-white font-weight-bold" :href="newsItem.url" target="_blank">Read</a></button>
       </div>
     </div>
   </div>
@@ -47,13 +49,42 @@ export default {
 </script>
 
 <style>
-img{
-  object-fit: cover;
-  width: 100%;
-  height: 100%;
+.card-title, .card-category{
+  margin: 0;
+}
+.card-category:nth-child(3){
+  margin-bottom: 1rem;
+}
+.card-title:nth-child(4){
+  margin-bottom: 1rem;
+}
+h4.card-title{
+  font-size: 2.5rem;
+  margin: 0.75rem 0;
+}
+.card-title:nth-child(2){
+  margin-bottom: 1rem;
+}
+
+button{
+  margin-top: 0.5rem;
+}
+button:hover{
+
 }
 .img-section{
-  width: 500px;
-  height: 500px;
+  width: 100%;
+}
+img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 0;
+}
+.news-item{
+  margin: 4rem auto;
+  max-width: 850px;
+  min-width: 850px;
+  margin-top: 0;
 }
 </style>
